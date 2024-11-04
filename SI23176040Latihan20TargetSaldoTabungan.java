8637ee9d571357d5923665642afe59d485624553
Salim <saliimrezaa@gmail.com>
2024-11-04 22:23:45 +0700

    SI23176040Latihan20TargetSaldoTabungan.java

--- SI23176040Latihan20TargetSaldoTabungan.java
+++ SI23176040Latihan20TargetSaldoTabungan.java
@@ -11,7 +11,7 @@ package si.pkg23176040.latihan20.targetsaldotabungan;
  * KELAS    : Sistem Informasi
  * NIM      : 23176040
  * Deskripsi Program : Program ini berisi untuk menghitung pertumbuhan saldo tabungan 
- * dengan bunga 8% per bulan hingga mencapai saldo target Rp. 6.000.0
+ * dengan bunga 8% per bulan hingga mencapai saldo target Rp. 6.000.000
  */
 public class SI23176040Latihan20TargetSaldoTabungan {
     public static void main(String[] args) {
